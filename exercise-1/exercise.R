1564b127d0ee33aca94d8791fe3b66a7d5561b48
Anukriti Goyal <Nikki@D-173-250-235-30.dhcp4.washington.edu>
2017-04-11 14:09:31 -0700

    A

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,20 +2,22 @@
 
 # Create a vector of the number of points the Seahawks scored the first 4 games of last season
 # Hint: (google "Seahawks scores 2016")
+seahawk.points <- c(36, 10, 12, 3)
 
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 4 games
-
+opponent.points <- c(6, 9, 10, 9)
 
 # Combine your two vectors into a dataframe
-
+game.points <- data.frame(seahawk.points, opponent.points)
 
 # Create a new column "diff" that is the difference in points
-
+game.points$diff <- c(abs(seahawk.points - opponent.points))
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+game.points$won <- c(seahawk.points > opponent.points)
 
 # Create a vector of the opponents
-
+opponent.names <- c('Cardinal', 'Vikings', 'Dolphins', 'Rams')
 
 # Assign your dataframe rownames of their opponents
+rownames(game.points) <- opponent.names
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,25 +1,25 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employees <- c(paste('Employee', 1:100))
 
 # Create a vector of 2014 salaries using the runif function
-
+old.salaries <- runif(100, 1000, 20000)
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+new.salaries <- runif(100, 5000, 40000)
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(employees, old.salaries, new.salaries)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- c(new.salaries - old.salaries)
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- c(new.salaries > old.salaries)
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+salary57 <- salaries[7, 'new.salaries'] 
 
 # How many employees got a raise?
 
